9141acdabd2b89629c85f35752752ece53c7c400
Pranav Komarla <pranavkv03@gmail.com>
2024-09-04 01:51:19 -0400

    first commit, still have to complete email sending and github/linkedin linking

---
+++ components/Stairs.jsx
@@ -0,0 +1,28 @@
+import { motion } from 'framer-motion'
+
+import React from 'react'
+
+const stairAnimation = {
+    initial: {
+        top: "0%",
+    },
+    animate: {
+        top: "100%",
+    },
+    exit: {
+        top: ["100%", "0%"],
+    },
+}
+
+const reverseIndex = (index) => {
+
+}
+
+const Stairs = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Stairs
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    darkMode: ['class'],
+    // content: [
+      
+    //   './pages/**/*.{js, jsx}',
+    //   './components/**/*.{js, jsx}',
+      
+    //   './src/**/*.{js,jsx}'
+    // ],
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  
+      // Or if using `src` directory:
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+	theme: {
+    	container: {
+    		center: 'true',
+    		padding: '15px'
+    	},
+    	screens: {
+    		sm: '640px',
+    		md: '768px',
+    		lg: '960px',
+    		xl: '1200px'
+    	},
+    	fontFamily: {
+    		primary: 'var(--font-jetbrainsMono)'
+    	},
+    	extend: {
+    		colors: {
+    			primary: '#1c1c22',
+    			accent: {
+    				DEFAULT: '#00ff99',
+    				hover: '#00e187'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
     },
-  },
-  plugins: [],
-};
+	plugins: [require("tailwindcss-animate")]
+  }
